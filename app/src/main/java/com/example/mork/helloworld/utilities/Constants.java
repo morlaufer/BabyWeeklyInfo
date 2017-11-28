5257b0d1e6da8226670f9c5c1697bedc128a4dea
morkarmon <mor.karmon@telefonica.com>
2017-11-28 15:36:25 +0200

    add Constats

---
+++ app/src/main/java/com/example/mork/helloworld/utilities/Constants.java
@@ -0,0 +1,9 @@
+package com.example.mork.helloworld.utilities;
+
+/**
+ * Created by mork on 11/28/17.
+ */
+
+public final class Constants {
+    public static final String PREFS_NAME = "myPrefsName";
+}
